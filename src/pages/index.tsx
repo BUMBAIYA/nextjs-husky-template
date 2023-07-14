d404716b4efc47818f136c45f89793979ab3c444
BUMBAIYA <amitchauhan263871@gmail.com>
2023-07-14 11:57:09 +0530

    hooked font to markup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="container mx-auto">
+    <main className={`${inter.className} container mx-auto`}>
       <h1 className="text-3xl">Nextjs Husky Tailwindcss Template</h1>
     </main>
   );
